b601f26d2cdd148fbba78d5a191a9bf3fd45a6cc
Danny Matthews <danny.matthews@crunch.co.uk>
2022-12-01 19:31:08 +0000

    Day 1 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,25 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return splitByElf(input).max()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return splitByElf(input).sortedDescending().take(3).sum()
     }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     println(part1(input))
     println(part2(input))
+}
+
+fun splitByElf(input: List<String>): List<Int> {
+    var allElvesCalorieList = input
+    val elfCaloriesList = mutableListOf<Int>()
+
+    while (allElvesCalorieList.isNotEmpty()) {
+        elfCaloriesList.add(allElvesCalorieList.takeWhile { it.isNotEmpty()}.sumOf { it.toInt() })
+        allElvesCalorieList = allElvesCalorieList.dropWhile { it.isNotEmpty() }.drop(1)
+    }
+
+    return elfCaloriesList
 }
